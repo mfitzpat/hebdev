ec95cfc6f863a806826969168df51969b187ab9c
BigwillystyleBeatz <wsheper@trinity.edu>
2011-10-07 16:03:59 -0500

    NewBase

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -8,11 +8,7 @@ public class BasicTest extends UnitTest
     @Test
     public void creatandRetrieveCustomer()
     {
-        new Customer("Will", "Shepherd", 0045).save();
-        Customer will = Customer.find("bylastName", "Shepherd").first();
-        
-        assertNotNull(will);
-        assertEquals("Will", will.firstName);
+        System.out.println("This is a test");
     }
 
 }
