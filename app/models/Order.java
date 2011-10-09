55572b7c8ffefdc13833a648add324a5c147a534
BigwillystyleBeatz <wsheper@trinity.edu>
2011-10-09 17:02:22 -0500

    Editing Order class

--- app/models/Order.java
+++ app/models/Order.java
@@ -4,6 +4,7 @@
  */
 package models;
 
+import java.util.ArrayList;
 import play.db.jpa.Model;
 import java.util.Date;
 import javax.persistence.Entity;
@@ -22,8 +23,6 @@ public class Order extends Model
     public int orderID;
     public int storeID;
     public int itemCount;
-    public int upc;
-    public int itemQuantity;
     public double totalRetailAmount;
     public double totalSalesAmount;
     public double totalDiscountAmount;
@@ -33,5 +32,41 @@ public class Order extends Model
     public char taxFlag;
     public char foodStampFlag;
     public char adFlag;
+    public ArrayList<Integer> itemQuantity;
+    public ArrayList<Integer> upc;
     public Date orderDate = new Date();
+    
+    public Order(int OrderID,int storeID, int itemCount, int upc, int itemQuantity,
+    double totalRetailAmount, double totalSalesAmount, double totalDiscountAmount,
+    double totalTaxAmount, char orderLocation, char weightFlag, char foodStampFlag,
+    char adFlag, Date orderDate)
+    {
+        
+        
+    }
+    
+    //Returns true if ID > 0
+    private boolean validID(int ID)
+    {
+        boolean check = false;
+        
+        if (ID > 0) 
+            check = true;
+        else
+            return check;
+        
+        return check;
+    }
+    
+    private boolean validAmount(double amount)
+    {
+        boolean check = false;
+        
+        if (amount > 0)
+            check = true;
+        else
+            return check;
+        
+        return check;
+    }
 }
